ced9f5e46334c7a2e8a3f6a0713958f569e01c4f
Wow Rakibul <74898633+wowrakibul@users.noreply.github.com>
2025-01-15 19:33:32 +0600

    Add files via upload

--- code.ts
+++ code.ts
@@ -33,5 +33,7 @@ figma.ui.onmessage =  (msg: {type: string, count: number}) => {
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+
+  // Uncomment this line to close the plugin when the user creates rectangles.
+  // figma.closePlugin();
 };
